40c33b21fb1f9f97374a01408d192f2060b6cb7e
Mizuki Kasuya <k1810169@edu.cc.uec.ac.jp>
2021-03-10 12:38:16 +0900

    use heroku sheduler

--- main.py
+++ main.py
@@ -6,19 +6,31 @@ import Content
 import Twitter_util
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+def main():
+    Leo = Content.FortuneTelling("leo")
+    LeoTweet = Twitter_util.TwitterUtil()
+    LeoTweet.Post(Leo.WorkText())
+    LeoTweet.Post(Leo.MoneyText())
+    LeoTweet.Post(Leo.LoveText())
+    LeoTweet.Post(Leo.TotalText())
+    LeoTweet.Post(Leo.Text())
+
+if __name__ == '__main__':
+    main()
+
+
+"""
 twische = BlockingScheduler()
 
 @twische.scheduled_job('interval', minutes=360)
 def main():
     Leo = Content.FortuneTelling("leo")
     LeoTweet = Twitter_util.TwitterUtil()
-    """
     print(Leo.WorkText())
     print(Leo.MoneyText())
     print(Leo.LoveText())
     print(Leo.TotalText())
     print(Leo.Text())
-    """
     LeoTweet.Post(Leo.WorkText())
     LeoTweet.Post(Leo.MoneyText())
     LeoTweet.Post(Leo.LoveText())
@@ -27,3 +39,4 @@ def main():
 
 if __name__ == '__main__':
     twische.start()
+"""
